c2c3d05b868a5ecdc3f03b9b578ba4c623da764e
Trading120 <trading120@users.noreply.github.com>
2026-04-10 01:54:57 +0800

    Add personal fork note to utils __init__.py

--- src/vunnel/utils/__init__.py
+++ src/vunnel/utils/__init__.py
@@ -1,3 +1,4 @@
+# Personal fork of anchore/vunnel - customized for local use
 from vunnel.utils import archive, fs, http, oval_parser, retry
 
 __all__ = ["archive", "fs", "http", "oval_parser", "retry"]
